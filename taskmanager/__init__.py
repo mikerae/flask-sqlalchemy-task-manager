ba6c9b81e051f3b94f78096f9a1c8ea22688ab3f
Mike Rae <mikerae@me.com>
2022-11-07 12:12:31 +0000

    Create taskmanager database

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,7 +7,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URL"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
